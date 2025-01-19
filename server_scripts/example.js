52c2ac158c9e78ff57fb1ae79b0b5d123eb21202
Adrien Chabert <adrien.chabert@univ-tlse3.fr>
2025-01-19 15:35:47 +0100

    MAJ

--- server_scripts/example.js
+++ server_scripts/example.js
@@ -2,21 +2,12 @@
 
 // Visit the wiki for more info - https://kubejs.com/
 BlockEvents.leftClicked(event => {
-    console.log(event.block)
-    console.log(event.block.y)
-    console.log(event.item)
     if (event.item == "kubejs:bedrock_breaker"){
-        event.level.tell("BB")
         if (event.block == "minecraft:bedrock"){
-            event.level.tell("Bedrock")
             if ( event.block.y > 122 && event.block.y <= 128){
-                event.level.tell("height")
-                let temp = new BlockPos
-                temp.x = event.block.x
-                temp.z = event.block.z
-                for (let y = 123; i <= 128; i++){
-                    temp.y = y
-                    event.level.destroyBlock(temp,false)
+                for (let y = 123;y<=127;Y++){
+                    dblock = new BlockPos(event.block.x,y,event.block.z)
+                    event.level.destroyBlock(dblock)            
                 }
             }
         }
