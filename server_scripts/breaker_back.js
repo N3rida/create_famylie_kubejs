2ffc28337ab1397572bcccecac0e66e10df96ac7
Adrien Chabert <adrien.chabert@univ-tlse3.fr>
2025-01-19 16:38:31 +0100

    Modification recette

--- server_scripts/breaker_back.js
+++ server_scripts/breaker_back.js
@@ -13,14 +13,18 @@ BlockEvents.leftClicked(event => {
 })
 ServerEvents.recipes(event => {
     event.recipes.create.mechanical_crafting('kubejs:bedrock_breaker', [
+      '       ',
+      '       ',
       '  PAND ',
       'PPPNSND',
-      '  PAND '
+      '  PAND ',
+      '       ',
+      '       ',
     ], {
       D: 'create:mechanical_drill',
       N: 'minecraft:netherite_ingot',
       S: 'minecraft:nether_star',
       A: 'create:andesite_alloy',
-      P: 'create:fluid_pipe'
+      P: 'create:fluid_pipe',
     })
   })
