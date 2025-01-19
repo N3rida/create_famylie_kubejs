8cf7906bf0bb61b630fa7fbac708d1fc710dc0d1
Adrien Chabert <adrien.chabert@univ-tlse3.fr>
2025-01-19 14:46:23 +0100

    MAJ

--- server_scripts/example.js
+++ server_scripts/example.js
@@ -3,4 +3,8 @@
 // Visit the wiki for more info - https://kubejs.com/
 BlockEvents.leftClicked(event => {
     console.log(event.block)
+    console.log(event.block.x)
+    console.log(event.block.y)
+    console.log(event.block.z)
+    console.log(event.item)
 })
