b4ac6489e1dfd83d3ea6d579b00d05b5cdf1347d
Adrien Chabert <adrien.chabert@univ-tlse3.fr>
2025-01-19 14:51:46 +0100

    MAJ

--- server_scripts/example.js
+++ server_scripts/example.js
@@ -3,8 +3,12 @@
 // Visit the wiki for more info - https://kubejs.com/
 BlockEvents.leftClicked(event => {
     console.log(event.block)
-    console.log(event.block.x)
     console.log(event.block.y)
-    console.log(event.block.z)
     console.log(event.item)
+    if (event.item == "bedrock_breaker" && 
+        event.block == "minecraft:bedrock" && 
+        event.block.y > 122 && 
+        event.block.y < 130){
+            event.level.tell("Ouii!")
+    }
 })
